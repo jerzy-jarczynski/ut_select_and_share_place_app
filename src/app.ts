5a4d0b7371467d1be58817c2744709bab25c542d
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-12-04 18:13:43 +0100

    Complete 171. Setting Up a Google API Key

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyD2iPtl2gKXU8FDwKY8XPODslEQHkfIT_k';
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
